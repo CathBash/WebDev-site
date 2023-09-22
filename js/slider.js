d80e88e49aa7f23de718e64253c6a3e7ea0b8122
Kate <bashta1801@mail.ru>
2023-09-22 17:03:41 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+var TariffSlider = new Swiper(".tariff-slider", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	loop: true,
+	slidesPerView: "auto",
+	coverflowEffect: {
+		rotate: 0,
+		stretch: 0,
+		depth: 100,
+		modifier: 2.5,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
